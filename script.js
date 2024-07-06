4ff5380030c8f7e4439d23f7b13423e3ee208bba
Amis <132851046+Ami-sDev@users.noreply.github.com>
2024-07-06 16:07:53 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const sidebar = document.querySelector('nav'),
-    toggle = document.querySelector('.toggle')
-
-//-------------------------------------------------------------------------------------------------//
-
-toggle.addEventListener('click' , () => {
-    sidebar.classList.toggle('close');
-})
-
-//-------------------------------------------------------------------------------------------------//
 
