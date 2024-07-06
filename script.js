d3f0a91a1a7511501c70ebf4e0769256e091d3d3
Amis <132851046+Ami-sDev@users.noreply.github.com>
2024-07-06 16:12:22 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const sidebar = document.querySelector('nav'),
+    toggle = document.querySelector('.toggle')
+
+//-------------------------------------------------------------------------------------------------//
+
+toggle.addEventListener('click' , () => {
+    sidebar.classList.toggle('close');
+})
+
+//-------------------------------------------------------------------------------------------------//
+
+const tel = window.Telegram.WebApp
+
+console.log(tel.initData)
+
 
