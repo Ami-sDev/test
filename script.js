c68fe83b690aa7335901c53cf301f3c26a8fb503
Amis <132851046+Ami-sDev@users.noreply.github.com>
2024-07-10 18:26:14 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const sidebar = document.querySelector('nav'),
-    toggle = document.querySelector('.toggle')
+const ranges = {
+    '2': {low: '000-499', high: '500-999'},
+    '5': {low: '000-199', high: '800-999'},
+    '10': {low: '000-099', high: '900-999'},
+    '20': {low: '000-049', high: '950-999'},
+    '100': {low: '000-010', high: '990-999'},
+    '1000': {low: '000', high: '999' }
+};
 
-//-------------------------------------------------------------------------------------------------//
+/*-------------------------------------------------------------------------------------------------*/
 
-toggle.addEventListener('click' , () => {
-    sidebar.classList.toggle('close');
-})
+$('.multiply-button').change(function() {
+    $('.color-ck3fv').text($(this).val() + " times");
+});
 
-//-------------------------------------------------------------------------------------------------//
+$('.multiply-button, .radio-button').change(function () {
+    const multiplyValue = $('input[name = "multiply"]:checked').val();
+    const rollValue = $('input[name = "roll"]:checked').attr('id');
+    const range = ranges[multiplyValue][rollValue === 'roll-low' ? 'low' : 'high'];
+    $('.color-c7gKa').text(range);
+});
+
+/*-------------------------------------------------------------------------------------------------*/
+
+
+
+/*-------------------------------------------------------------------------------------------------*/
